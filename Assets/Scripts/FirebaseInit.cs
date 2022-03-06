92fd1440fc73dcd1bc7add6807744a9d30236e22
Rahul Yadav <devmethyl@outlook.com>
2022-03-06 14:28:16 +0530

    Added Google Admob

--- Assets/Scripts/FirebaseInit.cs
+++ Assets/Scripts/FirebaseInit.cs
@@ -1,9 +1,11 @@
 using Firebase;
 using Firebase.Analytics;
+using GoogleMobileAds.Api;
 using UnityEngine;
 
 public class FirebaseInit : MonoBehaviour
 {
+    private BannerView bannerView;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +13,28 @@ public class FirebaseInit : MonoBehaviour
         {
             FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
         });   
+
+        MobileAds.Initialize(initStatus => { });
+        this.RequestBanner();
     }
 
+    private void RequestBanner()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
 }
