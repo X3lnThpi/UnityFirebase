142e9c6489d448d5933ae30990d0b7fe22573a92
Rahul Yadav <devmethyl@outlook.com>
2022-07-05 23:17:30 +0530

    Added Firebase Firestore Plugin

---
+++ Assets/Scripts/TestDB.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Firebase.Database;
+
+public class TestDB : MonoBehaviour
+{
+    [SerializeField] TMP_Text question;
+
+    
+}
